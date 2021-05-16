256a5553b5c8da8c00d46e7300ea5ee7fc60b298
liguopeng <liguopeng@liguopeng.net>
2021-05-16 22:29:36 +0800

    add format exception in gerrors

--- utils/gerrors.py
+++ utils/gerrors.py
@@ -3,11 +3,19 @@
 # created: 2015-05-14
 
 """异常和错误处理"""
-
+import sys
 import traceback
 
 
 def format_exception_stack():
     stack = traceback.format_exc()
-    return ''.join(stack)
+    return stack
 
+
+def format_exception(exception=None):
+    if exception:
+        stack = traceback.format_exception(type(exception), exception, exception.__traceback__)
+    else:
+        stack = traceback.format_exception(sys.last_type, sys.last_value, sys.last_traceback)
+
+    return ''.join(stack)
