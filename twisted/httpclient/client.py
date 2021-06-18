9789ad5a2c6dfdf25dbf44491d1368665c479c37
gy8409 <gy8409@gmail.com>
2021-06-18 12:35:49 +0800

    Fix: http request encoding wrong.

--- twisted/httpclient/client.py
+++ twisted/httpclient/client.py
@@ -46,7 +46,7 @@ class HTTPResponse(proxyForInterface(IResponse)):
     def raw_content(self):
         if not self.raw_body:
             self.raw_body = yield readBody(self.original)
-        returnValue(self.raw_body)
+        returnValue(self.raw_body.decode('utf-8'))
 
 
 class HTTPClient(object):
@@ -63,15 +63,17 @@ class HTTPClient(object):
         return self.request('GET', url, **kwargs)
 
     def post(self, url, data=None, **kwargs):
-        return self.request(bytes('POST', 'utf-8'), url, data=data, **kwargs)
+        return self.request('POST', url, data=data, **kwargs)
     
     def post_json(self, url, data, extra_headers=None, timeout=None):
         """data is a dict or list"""
         data = json.dumps(data).encode('utf-8')
-        url = bytes(url, 'utf-8')
         extra_headers = extra_headers or {}
         extra_headers.update({'content-type': self.Content_Type_JSON})
-        return self.post(url, data, **extra_headers)
+        if timeout:
+            extra_headers.update({'timeout': timeout})
+            
+        return self.request('POST', url, data=data, **extra_headers)
 
     def put(self, url, data=None, **kwargs):
         return self.request('PUT', url, data=data, **kwargs)
@@ -89,6 +91,13 @@ class HTTPClient(object):
 
         # Get body producer for POST and PUT
         body_producer = None
+        
+        if isinstance(method, str):
+            method = method.encode('utf-8')
+            
+        if isinstance(url, str):
+            url = url.encode('utf-8')
+
         data = kwargs.get('data')
         if data:
             if isinstance(data, JsonObject):
@@ -105,6 +114,17 @@ class HTTPClient(object):
 
 # Test
 if __name__ == "__main__":
+    @inlineCallbacks
+    def test():
+        # response = yield HTTPClient(Agent(reactor)).get('https://www.baidu.com')
+        response = yield HTTPClient(Agent(reactor)).post('http://127.0.0.1:9999')
+        # response = yield HTTPClient(Agent(reactor)).post_json('http://127.0.0.1:9999', data=JsonObject({'a': 'b'}))
+        response = yield response.raw_content()
+        print(type(response))
+        print(response)
+        
+    reactor.callLater(0, test)
+    reactor.run()
     pass
 
 
