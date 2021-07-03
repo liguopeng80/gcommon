ef78462c644e1e859477f6950756b65697c9eaae
liguopeng <liguopeng@liguopeng.net>
2021-07-03 17:47:11 +0800

    create  generalserver configg parser - from yaml

---
+++ server/server_config.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*- 
+# created: 2021-06-29
+# creator: liguopeng@liguopeng.net
+
+from copy import copy
+
+from gcommon.utils.gjsonobj import JsonObject
+
+
+class ServerConfig(object):
+    server_address = ""
+    server_port = ""
+
+    username = ""
+    password = ""
+
+    def clone(self):
+        return copy(self)
+
+    @classmethod
+    def load(cls, config: JsonObject):
+        self = cls()
+
+        self.server_address = config.server_address
+        self.server_port = config.server_port
+
+        self.username = config.username
+        self.password = config.password
+
+        self._load_extra_config(config)
+
+        return self
+
+    def _load_extra_config(self, config: JsonObject):
+        """派生类自定义的配置属性"""
+        pass
