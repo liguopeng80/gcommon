46b853d3956750872231e2870b98bb48a655c709
liguopeng <liguopeng@liguopeng.net>
2021-07-04 21:25:34 +0800

    add gflask for flask and quart

---
+++ aio/gflask.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*- 
+# created: 2021-07-04
+# creator: liguopeng@liguopeng.net
+
+from gcommon.utils.gglobal import Global
+
+
+def register_blueprint(app, module):
+    server_base = Global.config.get("service.web.base_url")
+    module_base = server_base + module.base_url
+
+    app.register_blueprint(module.app, url_prefix=module_base)
+
