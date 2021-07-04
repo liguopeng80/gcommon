2dc45ba5e0cdda0ad5ff05c77ee0d24fd8ad0398
liguopeng <liguopeng@liguopeng.net>
2021-07-04 21:16:36 +0800

    add AsyncTimer in aio

--- aio/gtimer.py
+++ aio/gtimer.py
@@ -7,86 +7,73 @@
 import asyncio
 
 import logging
+from datetime import datetime
+
+import typing
+
 logger = logging.getLogger('timer')
 
 
 class AsyncTimer(object):
-    @staticmethod
-    def start(seconds):
-        d = Deferred()
-        reactor.callLater(seconds, d.callback, None)
-        return d
-
-    @staticmethod
-    async def wait(seconds):
-        """ wait until interrupted by callback or timeout that will raise a CancelledError """
-        d = Deferred()
-        _delayed_call = reactor.callLater(seconds, timeout, d)
-        d._gcommon_delayed_call = _delayed_call
-
-        def _on_finished(result):
-            logger.debug(">>> AsyncTimer _on_finished: %s, defer: %s", result, d)
-            if _delayed_call.active():
-                logger.debug("--- AsyncTimer.wait, job done! cancel timer.")
-                _delayed_call.cancel()
-
-            return result
-
-        d.addBoth(_on_finished)
-
-        return d
-
-    @staticmethod
-    def cancel_timer(d):
-        d._gcommon_delayed_call.cancel()
-
-
-class Timer(object):
     Not_Started = 0
     Started = 1
     Timed_Out = 2
     Cancelled = 3
-    
-    def __init__(self, seconds, timeout_handler, *args, **kwargs):
-        self.status = self.Not_Started
-        
-        self.seconds = seconds
 
+    def __init__(self, timeout_handler, *args, **kwargs):
+        self.status = self.Not_Started
+
+        self.seconds = 0
         self.timeout_handler = timeout_handler
         self._args = args
         self._kwargs = kwargs
-        
+
         self._task = None
-        
-    async def start(self):
+
+    def start(self, seconds: int = 0, dt: datetime = None):
         if self.status != self.Not_Started:
             return None
 
+        self.seconds = self._calc_timeout(seconds, dt)
+
+        # 启动异步函数
         self.status = self.Started
         self._task = asyncio.ensure_future(self._job())
 
+    @staticmethod
+    def _calc_timeout(seconds: int, dt: datetime):
+        if dt:
+            seconds = (dt - datetime.now()).total_seconds()
+            if seconds < 0:
+                seconds = 0
+
+        return seconds
+
     async def _job(self):
         await asyncio.sleep(self.seconds)
-
         if self.status == self.Started:
-            await self.timeout_handler(*self._args, **self._kwargs)
+            self.status = self.Timed_Out
+            # await self.timeout_handler(*self._args, **self._kwargs)
+            # maybe_async
+            self.timeout_handler(*self._args, **self._kwargs)
 
-    def restart(self, seconds = 0):
-        if seconds:
-            self.seconds = seconds
-            
+    def restart(self, seconds: int = 0, dt: datetime = None):
+        self.seconds = self._calc_timeout(seconds, dt)
+
         if self.status == self.Started:
             self.cancel()
-            
+        elif self.status == self.Timed_Out:
+            # todo: 已经超时，怎么处理？
+            pass
+
         self.status = self.Not_Started
         self.start()
-        
+
         return self
-        
+
     def cancel(self):
         if self.status != self.Started:
             return
-            
+
         self.status = self.Cancelled
         self._task.cancel()
-
--- aio/test/test_gtimer.py
+++ aio/test/test_gtimer.py
@@ -2,3 +2,22 @@
 # created: 2021-07-04
 # creator: liguopeng@liguopeng.net
 
+import asyncio
+from gcommon.aio.gtimer import AsyncTimer
+
+
+def timeout_callback(f: asyncio.Future):
+    print("timeout")
+    f.set_result("Done")
+
+
+async def test():
+    f = asyncio.Future()
+    AsyncTimer(timeout_callback, f).start(0.1)
+
+    result = await f
+    print(result)
+
+
+if __name__ == '__main__':
+    asyncio.run(test())
