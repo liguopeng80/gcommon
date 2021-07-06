16251bc8c538004d93d8681abf1e7a1a9b6bc8f8
liguopeng <liguopeng@liguopeng.net>
2021-07-06 13:42:43 +0800

    fix bug: base_url 可能没有设置

--- aio/gflask.py
+++ aio/gflask.py
@@ -6,7 +6,7 @@ from gcommon.utils.gglobal import Global
 
 
 def register_blueprint(app, module):
-    server_base = Global.config.get("service.web.base_url")
+    server_base = Global.config.get("service.web.base_url") or ""
     module_base = server_base + module.base_url
 
     app.register_blueprint(module.app, url_prefix=module_base)
