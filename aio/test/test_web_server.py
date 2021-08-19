48e23d1b062dc26a29a12caadf4d83442b6acf6f
liguopeng <liguopeng@liguopeng.net>
2021-08-19 11:00:18 +0800

    test: add test web server

---
+++ aio/test/test_web_server.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*- 
+# created: 2021-08-19
+# creator: liguopeng@liguopeng.net
+
+from quart import Quart, websocket, request
+
+app = Quart(__name__)
+
+
+@app.route('/')
+@app.route('/', methods=["POST"])
+async def hello():
+    body = await request.get_data()
+    return body
+
+
+@app.websocket('/ws')
+async def ws():
+    while True:
+        await websocket.send('hello')
+
+app.run(host="127.0.0.1", port=12580)
