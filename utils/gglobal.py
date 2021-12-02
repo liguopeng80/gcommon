1d4430950b786efc400a773acd550b6a3f0bff0d
liguopeng <liguopeng@liguopeng.net>
2021-12-02 11:04:51 +0800

    feature: 在 global 中增加服务的工作模式 working_mode

--- utils/gglobal.py
+++ utils/gglobal.py
@@ -6,6 +6,10 @@ from gcommon.utils.gyaml import YamlConfigParser
 
 
 class Global(object):
+    # 当前进程的工作模式（如果有多种模式），空串代表不支持多模式切换，或采用缺省模式
+    working_mode = ""
+
+    # 从环境变量、命令行、配置文件加载的配置
     config = YamlConfigParser()
 
     @classmethod
