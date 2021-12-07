469d5c4c22c7272bed167722ab97ed34295d5740
liguopeng <liguopeng@liguopeng.net>
2021-12-07 16:15:26 +0800

    feature: add grpc exception

---
+++ aio/grpc_utils.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*- 
+# created: 2021-12-06
+# creator: liguopeng@liguopeng.net
+
+"""General RPC Exceptions"""
+
+
+class GeneralRpcException(Exception):
+    """RPC 异常的基类"""
+    pass
+
+
+class RpcBadRouteException(GeneralRpcException):
+    """路由错误，客户端应该检查路由设置，重发请求到正确的服务器"""
+    pass
+
