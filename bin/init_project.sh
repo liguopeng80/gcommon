f58bc604154a5e26dcc408fd9c4fffa50cf55437
liguopeng <liguopeng@liguopeng.net>
2022-01-10 21:22:56 +0800

    feat: add project init script

---
+++ bin/init_project.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+mkdir deploy
+touch deploy/default.yaml
+
+mkdir docs
+touch docs/README.md
+
+mkdir data
+touch data/README.md
+
+mkdir log
+touch log/README.md
+
+mkdir bin
+touch bin/README.md
