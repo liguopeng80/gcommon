61dde79f04de1ef720f46a45c8fd005a6fc3e3bc
liguopeng <liguopeng@liguopeng.net>
2022-07-06 17:31:23 +0800

    fix: wrong mac address length

--- gcommon/utils/gos.py
+++ gcommon/utils/gos.py
@@ -5,9 +5,12 @@
 from uuid import getnode as get_mac
 
 
-def get_mac_address():
+def get_mac_address(sep=":"):
     mac = get_mac()
     mac_str = "%x" % mac
 
-    mac_bytes = [mac_str[i: i + 2] for i in range(0, int(len(mac_str) / 2), 2)]
-    return ":".join(mac_bytes)
+    if sep:
+        mac_bytes = [mac_str[i: i + 2] for i in range(0, int(len(mac_str)), 2)]
+        return sep.join(mac_bytes)
+    else:
+        return mac_str
